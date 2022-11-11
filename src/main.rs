386618dae507fed6d1a0cf194a1da86364255a7b
Michael Osuna Jr <mikeosunajr@gmail.com>
2022-11-10 17:30:38 -0800

    aaaaaaa

--- src/main.rs
+++ src/main.rs
@@ -1,165 +1,93 @@
-use std::{fmt, iter::Peekable, str::CharIndices};
+use std::sync::Arc;
 
 fn main() {}
+#[derive(Debug, PartialEq)]
+enum Context {}
+#[derive(Debug, PartialEq)]
+enum Problem {}
 
+#[derive(PartialEq, Debug)]
+enum Expression {
+    Empty,
+}
 #[cfg(test)]
 mod tests {
+
+    use crate::run;
+    use crate::{Context, Expression, Problem, State};
+
     #[test]
     fn it_works() {
-        let res = super::Parser::new("1 + 2 * 3").parse();
-        assert!(res.is_ok());
-        assert_eq!("(+ 1 (* 2 3))", res.unwrap().to_string());
+        assert_eq!(
+            Ok((Expression::Empty, State::new("1"))),
+            run::<Context, Problem, Expression>(
+                |state: &State<Context>| {
+                    let s = State::<Context>::new("1");
+                    Ok((Expression::Empty, s))
+                },
+                "1",
+            )
+        );
     }
+}
+
+impl<'a, 'b, F, Context, C2: 'b, Problem, Value> Parser<'a, 'b, Context, Problem, Value> for F
+where
+    F: Fn(&'a State<'a, Context>) -> PStep<'b, C2, Problem, Value>,
+{
+    fn parse(&self, state: &'a State<'a, Context>) -> PStep<'b, Context, Problem, Value> {
+        self(&state)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Located<Context> {
+    row: i64,
+    col: i64,
+    context: Context,
+}
+
+#[derive(Debug, PartialEq)]
+struct State<'a, Context: 'a> {
+    src: &'a str,
+    offset: i64,
+    indent: i64,
+    context: Vec<Located<Context>>,
+    row: i64,
+    col: i64,
 }
 #[derive(Debug, PartialEq)]
-enum Operator {
-    Multiply,
-    Sum,
-}
-#[derive(Debug)]
-enum ParseError {
-    InvalidInteger,
+struct DeadEnd<Context, Problem> {
+    row: i64,
+    col: i64,
+    problem: Problem,
+    context_stack: Vec<Located<Context>>,
 }
 
-type ParseResult = Result<Expression, ParseError>;
+type PStep<'a, Context, Problem, Value> =
+    Result<(Value, State<'a, Context>), Vec<DeadEnd<Context, Problem>>>;
 
-#[derive(Debug, PartialEq)]
-enum Expression {
-    Integer(u64),
-    BinaryOperation(Box<Expression>, Operator, Box<Expression>),
+trait Parser<'a, 'b, Context, Problem, Value> {
+    fn parse(&self, state: &'a State<'a, Context>) -> PStep<'b, Context, Problem, Value>;
 }
 
-fn infix_binding_power(op: &Operator) -> (u8, u8) {
-    match op {
-        Operator::Sum => (1, 2),
-        Operator::Multiply => (3, 4),
-    }
-}
-
-impl fmt::Display for Expression {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Expression::Integer(i) => write!(f, "{}", i),
-            Expression::BinaryOperation(lhs, op, rhs) => write!(f, "({} {} {})", op, lhs, rhs),
-        }
-    }
-}
-
-impl fmt::Display for Operator {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Operator::Sum => write!(f, "+"),
-            Operator::Multiply => write!(f, "*"),
-        }
-    }
-}
-
-struct Parser<'a> {
-    input: &'a str,
-    chars: Peekable<CharIndices<'a>>,
-}
-
-trait Scanner {
-    fn offset(&self) -> usize;
-    fn peek(&self) -> Option<char>;
-    fn next(&self) -> Option<char>;
-    fn slice(&self, start: usize) -> &str;
-}
-
-impl<'a> Parser<'a> {
-    pub fn new(str: &'a str) -> Self {
+impl<'a, Context> State<'a, Context> {
+    fn new(src: &'a str) -> Self {
         Self {
-            input: str,
-            chars: str.char_indices().peekable(),
+            src,
+            offset: 0,
+            indent: 1,
+            row: 1,
+            col: 1,
+            context: Vec::new(),
         }
     }
+}
 
-    pub fn parse(&mut self) -> ParseResult {
-        self.expression(0)
-    }
-
-    pub fn expression(&mut self, min_bp: u8) -> ParseResult {
-        self.ws();
-        let mut lhs = self.number()?;
-        self.ws();
-        loop {
-            if self.eof() {
-                break;
-            }
-            let op = self.op()?;
-            let (l_bp, r_bp) = infix_binding_power(&op);
-            if l_bp < min_bp {
-                break;
-            }
-            let rhs = self.expression(r_bp)?;
-
-            lhs = Expression::BinaryOperation(Box::new(lhs), op, Box::new(rhs));
-        }
-
-        Ok(lhs)
-    }
-
-    fn op(&mut self) -> Result<Operator, ParseError> {
-        match self.chars.next() {
-            Some((_, b)) => match b {
-                '+' => Ok(Operator::Sum),
-                '*' => Ok(Operator::Multiply),
-                _ => Err(ParseError::InvalidInteger),
-            },
-            None => Err(ParseError::InvalidInteger),
-        }
-    }
-
-    fn get_slice(&mut self, start: usize) -> &str {
-        match self.chars.peek() {
-            Some((end, _)) => &self.input[start..*end],
-            None => &self.input[start..],
-        }
-    }
-
-    fn ws(&mut self) {
-        while let Some((_, _ch)) = self.chars.peek() {
-            if !_ch.is_whitespace() {
-                break;
-            }
-            self.chars.next();
-        }
-    }
-
-    fn cc(&mut self) -> Option<char> {
-        self.chars.peek().map(|(_, c)| *c)
-    }
-
-    fn eof(&mut self) -> bool {
-        self.chars.peek().is_none()
-    }
-
-    fn number(&mut self) -> ParseResult {
-        match self.chars.peek() {
-            Some((_start, _ch)) => {
-                let start = *_start;
-                let mut ch = *_ch;
-                while ch.is_digit(10) {
-                    self.chars.next();
-
-                    if let Some(_ch) = self.cc() {
-                        ch = _ch;
-                    } else {
-                        break;
-                    }
-                }
-
-                let number_string = self.get_slice(start);
-                if number_string.is_empty() {
-                    return Err(ParseError::InvalidInteger);
-                }
-
-                number_string
-                    .parse::<u64>()
-                    .map(|num| Expression::Integer(num))
-                    .map_err(|_| ParseError::InvalidInteger)
-            }
-            None => Err(ParseError::InvalidInteger),
-        }
-    }
+fn run<'a, 'b, Context, Problem, Value>(
+    parser: impl Parser<'a, 'b, Context, Problem, Value>,
+    src: &'a str,
+) -> PStep<'b, Context, Problem, Value> {
+    let s = State::new(src);
+    parser.parse(&s)
 }
